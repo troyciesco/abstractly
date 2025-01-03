724fcd210b72f321363fdd368332883e980db3aa
Troy Ciesco <tmciesco@gmail.com>
2025-01-03 12:19:25 -0500

    try postcss instead of vite to fix menu btn css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config"
-import tailwindcss from "@tailwindcss/vite"
-
-import react from "@astrojs/react";
-
-import netlify from "@astrojs/netlify";
+// import tailwindcss from "@tailwindcss/vite"
+import react from "@astrojs/react"
+import netlify from "@astrojs/netlify"
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-      plugins: [tailwindcss()]
-    },
+	// vite: {
+	//     plugins: [tailwindcss()]
+	//   },
 
-  integrations: [react()],
-  adapter: netlify()
+	integrations: [react()],
+	adapter: netlify()
 })
