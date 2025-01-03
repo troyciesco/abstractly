86813f87c518c78fc747990f9eb68234979c668c
Troy Ciesco <tmciesco@gmail.com>
2025-01-03 15:24:44 -0500

    footer: add footer and astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,14 @@ import { defineConfig } from "astro/config"
 import react from "@astrojs/react"
 import netlify from "@astrojs/netlify"
 
+import icon from "astro-icon"
+
 // https://astro.build/config
 export default defineConfig({
 	// vite: {
 	//     plugins: [tailwindcss()]
 	//   },
 
-	integrations: [react()],
+	integrations: [react(), icon({ iconDir: "src/assets/icons" })],
 	adapter: netlify()
 })
--- src/lib/library-links.ts
+++ src/lib/library-links.ts
@@ -1,5 +1,6 @@
 export const libraryLinks = [
 	{ name: "Testimonal Card", slug: "testimonial-card" },
 	{ name: "Hero (Simple)", slug: "hero-simple" },
-	{ name: "Navbar", slug: "navbar" }
+	{ name: "Navbar", slug: "navbar" },
+	{ name: "Footer", slug: "footer-simple" }
 ]
