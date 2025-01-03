bb482d8f2161e32a807d490c06c6d3beb21d4889
Troy Ciesco <tmciesco@gmail.com>
2025-01-03 16:17:20 -0500

    footer, feature grid, various other setup

---
+++ src/lib/grid-features.ts
@@ -0,0 +1,38 @@
+export const gridFeatures = [
+	{
+		icon: "download",
+		title: "Infinite Download",
+		description:
+			"Once you subscribe to our plans, they're all yours. Download as many as you want and use them for work presentations, wallpapers, and much more."
+	},
+	{
+		icon: "brush",
+		title: "Purely Handcrafted",
+		description:
+			"No AI, no generic images. Crafted from various chemicals, fabrics, clouds, or even particles as small as dust."
+	},
+	{
+		icon: "copyright",
+		title: "All Are Under Licensed",
+		description:
+			"The only limitation with these abstract images is that you are not able to sell them in any form, whether digital or hard copy (such as paintings or prints on paper)."
+	},
+	{
+		icon: "currency",
+		title: "Cancel Anytime",
+		description:
+			"Subscribe at your own pace, and cancel when you feel it's enough."
+	},
+	{
+		icon: "team",
+		title: "Empowering for Team",
+		description:
+			"We support multiple seats at once, requiring only a single payment."
+	},
+	{
+		icon: "refresh",
+		title: "No Limitations",
+		description:
+			"Use as many as you want, from Dribbble presentations to PowerPoint presentations."
+	}
+]
--- src/lib/library-links.ts
+++ src/lib/library-links.ts
@@ -2,5 +2,7 @@ export const libraryLinks = [
 	{ name: "Testimonal Card", slug: "testimonial-card" },
 	{ name: "Hero (Simple)", slug: "hero-simple" },
 	{ name: "Navbar", slug: "navbar" },
-	{ name: "Footer", slug: "footer-simple" }
+	{ name: "Footer", slug: "footer-simple" },
+	{ name: "Feature Grid", slug: "feature-grid" },
+	{ name: "Feature Split", slug: "feature-split" }
 ]
