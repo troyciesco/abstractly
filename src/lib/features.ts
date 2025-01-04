1da0abbe9863866978c7ad03b428dc362de54c24
Troy Ciesco <tmciesco@gmail.com>
2025-01-04 14:58:38 -0500

    move to feature section, and start of new sections

---
+++ src/lib/features.ts
@@ -0,0 +1,85 @@
+export type Feature = {
+	icon: string
+	title: string
+	description: string
+}
+export const accessFeatures: Feature[] = [
+	{
+		icon: "download",
+		title: "Infinite Download",
+		description:
+			"Once you subscribe to our plans, they're all yours. Download as many as you want and use them for work presentations, wallpapers, and much more."
+	},
+	{
+		icon: "brush",
+		title: "Purely Handcrafted",
+		description:
+			"No AI, no generic images. Crafted from various chemicals, fabrics, clouds, or even particles as small as dust."
+	},
+	{
+		icon: "copyright",
+		title: "All Are Under Licensed",
+		description:
+			"The only limitation with these abstract images is that you are not able to sell them in any form, whether digital or hard copy (such as paintings or prints on paper)."
+	},
+	{
+		icon: "currency",
+		title: "Cancel Anytime",
+		description:
+			"Subscribe at your own pace, and cancel when you feel it's enough."
+	},
+	{
+		icon: "team",
+		title: "Empowering for Team",
+		description:
+			"We support multiple seats at once, requiring only a single payment."
+	},
+	{
+		icon: "refresh",
+		title: "No Limitations",
+		description:
+			"Use as many as you want, from Dribbble presentations to PowerPoint presentations."
+	}
+]
+
+export const designerFeatures: Feature[] = [
+	{
+		icon: "hd",
+		title: "5K resolution support",
+		description:
+			"All images boast a minimum resolution of 5K, ensuring crisp, crystal-clear quality."
+	},
+	{
+		icon: "droplet",
+		title: "From water to glass",
+		description:
+			"We offer a wide array of abstractions, ranging from water to glass, and encompassing various styles including 3D and vector."
+	},
+	{
+		icon: "arch",
+		title: "Portrait or landscape",
+		description:
+			"Effortlessly adapt your images for any platform - whether it's a stunning wallpaper or captivating Instagram reels and stories."
+	}
+]
+
+export const convenienceFeatures: Feature[] = [
+	{
+		icon: "rocket",
+		title: "Faster downloads",
+		description:
+			"Our robust servers are primed to deliver the highest resolution images swiftly, ensuring a smooth download experience."
+	},
+	{
+		icon: "pair",
+		title: "Convenience for teams",
+		description:
+			"Your single account can accommodate multiple users simultaneously downloading without any disruptions, streamlining teamwork and productivity."
+	},
+	{
+		icon: "copyright",
+		title: "Royalty-free licensing",
+		description:
+			"Our straightforward, royalty-free licensing means your chosen images are yours to innovate with, without the hassle of negotiating usage rights for every new project."
+	}
+]
